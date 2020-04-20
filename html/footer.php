18ce7b8db16739c0817f35b55c3c3c5db9972cf6
eli.stone <eli.stone@amazee.com>
2020-04-20 19:36:51 +0100

    Switched homepage over to bootstrap 4

--- html/footer.php
+++ html/footer.php
@@ -2,12 +2,22 @@
 // MillionDollarScript footer.php
 
 ?>
-			</div>
-			<div class="clear"></div>
-			<div class="footer">&copy; Copyright <?php echo date('Y') . " " . SITE_NAME; ?></div>
-		</div>
-		<div class="clear"></div>
-	</div>
+</div>
 
+<footer class="text-muted">
+    <div class="container py-4">
+        <p>&copy; Copyright <?php echo date('Y') . " " . SITE_NAME; ?></p>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 </body>
 </html>
--- html/header.php
+++ html/header.php
@@ -11,76 +11,85 @@ otherwise output the HTML to the browser.
 
 */
 
-if (MDS_AGRESSIVE_CACHE=='YES') {
+if (MDS_AGRESSIVE_CACHE == 'YES') {
 
-	// cache all requests, browsers must respect this php script
-	header('content-type: text/html; charset=utf-8');
-	header('Cache-Control: public, must-revalidate');
-	$if_modified_since = preg_replace('/;.*$/', '', $_SERVER['HTTP_IF_MODIFIED_SINCE']);
-	$gmdate_mod = gmdate('D, d M Y H:i:s', $b_row['time_stamp']) . ' GMT';
-	if ($if_modified_since == $gmdate_mod) {
-		header("HTTP/1.0 304 Not Modified");
-		exit;	
-	}
-	header("Last-Modified: $gmdate_mod");
+    // cache all requests, browsers must respect this php script
+    header('content-type: text/html; charset=utf-8');
+    header('Cache-Control: public, must-revalidate');
+    $if_modified_since = preg_replace('/;.*$/', '', $_SERVER['HTTP_IF_MODIFIED_SINCE']);
+    $gmdate_mod = gmdate('D, d M Y H:i:s', $b_row['time_stamp']) . ' GMT';
+    if ($if_modified_since == $gmdate_mod) {
+        header("HTTP/1.0 304 Not Modified");
+        exit;
+    }
+    header("Last-Modified: $gmdate_mod");
 
 }
 
-$BID = (isset($_REQUEST['BID']) && $f2->bid($_REQUEST['BID'])!='') ? $f2->bid($_REQUEST['BID']) : $BID = 1;
+$BID = (isset($_REQUEST['BID']) && $f2->bid($_REQUEST['BID']) != '') ? $f2->bid($_REQUEST['BID']) : $BID = 1;
 
+$logourl = SITE_LOGO_URL;
 ?><!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo SITE_NAME; ?></title>
-	<meta name="Description" content="<?php echo SITE_SLOGAN; ?>">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-	<link rel="stylesheet" type="text/css" href="main.css?ver=<?php echo filemtime(BASE_PATH . "/main.css"); ?>" >
+    <title><?php echo SITE_NAME; ?></title>
+    <meta name="Description" content="<?php echo SITE_SLOGAN; ?>">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--	<link rel="stylesheet" type="text/css" href="main.css?ver=-->
+    <?php //echo filemtime(BASE_PATH . "/main.css"); ?><!--" >-->
 </head>
-<body class="main">
+<body>
 
-	<div class="outer">
-		<div class="inner">
-		
-			<div class="heading">
-			
-				<?php
-				$logourl = SITE_LOGO_URL;
-				if(!empty($logourl)) { ?>
-				<!-- logo image -->
-				<div class="logo">
-					<a href="index.php">
-						<img src="<?php echo htmlentities($logourl); ?>" style="border:0px;" alt="" />
-					</a>
-				</div>
-				<?php } ?>
-				
-				<?php
-				$slogan = SITE_SLOGAN;
-				if(!empty($slogan)) { ?>
-				<!-- slogan -->
-				<div class="slogan">
-					<?php echo htmlentities($slogan); ?>
-				</div>
-				<?php } ?>
-				
-				<!-- stats iframe -->
-				<div class="status_outer">
-					<?php echo get_stats_html_code( $BID ); ?>
-				</div>
-				
-				<div class="clear"></div>
-			</div>
-			
-			<!-- menu links -->
-			<div class="menu_outer">
-				<div class="menu_bar">
-					<ul class="menu">
-						<li><a href='index.php'>Home</a></li>
-						<li><a href='users/'>Buy Pixels</a></li>
-						<li><a href='list.php'>Ads List</a></li>
-					</ul>
-					<div class="clear"></div>
-				</div>
-			</div>
-			<div class="container">
+<!--<a class="navbar-brand" href="index.php">-->
+<!--    --><?php //if (!empty($logourl)) { ?>
+<!--       <img src="--><?php ////echo htmlentities($logourl); ?><!--" class="d-inline-block align-top" alt="">-->
+<!--    --><?php //} ?>
+<!--    $500,000 Drupal Page-->
+<!--</a>-->
+
+<div class="jumbotron mb-0">
+    <div class="container text-center">
+        <h1>$500,000 Drupal Homepage</h1>
+    </div>
+</div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href='index.php'>Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href='users/'>Buy Pixels</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href='list.php'>Ads List</a>
+                </li>
+            </ul>
+            <?php echo get_stats_html_code($BID); ?>
+        </div>
+    </div>
+</nav>
+
+<div class="container text-center">
+
+    <?php
+    $slogan = SITE_SLOGAN;
+    if (!empty($slogan)) { ?>
+        <!-- slogan -->
+        <div class="slogan">
+            <?php // echo htmlentities($slogan); ?>
+        </div>
+    <?php } ?>
+
 			

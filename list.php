308390027004f60795f04f0c9acc88aa9b5bec23
eli.stone <eli.stone@amazee.com>
2020-04-20 22:01:11 +0100

    Converted list page to a table

--- list.php
+++ list.php
@@ -44,58 +44,62 @@ global $label;
 <?php include( MDSROOT . "/top_ads_js.php" ); ?>
 <?php include( 'mouseover_box.htm' ); ?>
 
-    <div class="list">
-        <div class="table-row header">
-            <div class="list-heading"><?php echo $label['list_date_of_purchase']; ?></div>
-            <div class="list-heading"><?php echo $label['list_name']; ?></div>
-            <div class="list-heading"><?php echo $label['list_ads']; ?></div>
-            <div class="list-heading"><?php echo $label['list_pixels']; ?></div>
-        </div>
-		<?php
-		require_once( "include/ads.inc.php" );
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col"><?php echo $label['list_date_of_purchase']; ?></th>
+            <th scope="col"><?php echo $label['list_name']; ?></th>
+            <th scope="col"><?php echo $label['list_ads']; ?></th>
+            <th scope="col"><?php echo $label['list_pixels']; ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        require_once( "include/ads.inc.php" );
 
-		$sql = "SELECT *, MAX(order_date) as max_date, sum(quantity) AS pixels FROM orders where status='completed' AND approved='Y' AND published='Y' AND banner_id='".intval($BID)."' GROUP BY user_id, banner_id, order_id order by pixels desc ";
-		$result = mysqli_query( $GLOBALS['connection'], $sql ) or die( mysqli_error( $GLOBALS['connection'] ) );
-		while ( $row = mysqli_fetch_array( $result ) ) {
-			$q = "SELECT FirstName, LastName FROM users WHERE ID=" . intval($row['user_id']);
-			$q = mysqli_query( $GLOBALS['connection'], $q ) or die( mysqli_error( $GLOBALS['connection'] ) );
-			$user = mysqli_fetch_row( $q );
-			?>
-            <div class="table-row">
-                <div class="list-cell">
-					<?php echo get_formatted_date( get_local_time( $row['max_date'] ) ); ?>
-                </div>
-                <div class="list-cell">
-					<?php echo $user['0'] . " " . $user['1']; ?>
-                </div>
-                <div class="list-cell">
-					<?php
+        $sql = "SELECT *, MAX(order_date) as max_date, sum(quantity) AS pixels FROM orders where status='completed' AND approved='Y' AND published='Y' AND banner_id='".intval($BID)."' GROUP BY user_id, banner_id, order_id order by pixels desc ";
+        $result = mysqli_query( $GLOBALS['connection'], $sql ) or die( mysqli_error( $GLOBALS['connection'] ) );
+        while ( $row = mysqli_fetch_array( $result ) ) {
+            $q = "SELECT FirstName, LastName FROM users WHERE ID=" . intval($row['user_id']);
+            $q = mysqli_query( $GLOBALS['connection'], $q ) or die( mysqli_error( $GLOBALS['connection'] ) );
+            $user = mysqli_fetch_row( $q );
+            ?>
+        <tr>
+                <td>
+                    <?php echo get_formatted_date( get_local_time( $row['max_date'] ) ); ?>
+                </td>
+                <td>
+                    <?php echo $user['0'] . " " . $user['1']; ?>
+                </td>
+                <td>
+                    <?php
 
-		                $br = "";
-		                $sql = "Select * FROM  `ads` as t1, `orders` AS t2 WHERE t1.ad_id=t2.ad_id AND t1.banner_id='".intval($BID)."' and t1.order_id='" . intval($row['order_id']) . "' AND t1.user_id='" . intval($row['user_id']) . "' AND status='completed' AND approved='Y' ORDER BY `ad_date`";
-		                $m_result = mysqli_query( $GLOBALS['connection'], $sql ) or die( mysqli_error( $GLOBALS['connection'] ) );
-		                while ( $prams = mysqli_fetch_array( $m_result, MYSQLI_ASSOC ) ) {
+                    $br = "";
+                    $sql = "Select * FROM  `ads` as t1, `orders` AS t2 WHERE t1.ad_id=t2.ad_id AND t1.banner_id='".intval($BID)."' and t1.order_id='" . intval($row['order_id']) . "' AND t1.user_id='" . intval($row['user_id']) . "' AND status='completed' AND approved='Y' ORDER BY `ad_date`";
+                    $m_result = mysqli_query( $GLOBALS['connection'], $sql ) or die( mysqli_error( $GLOBALS['connection'] ) );
+                    while ( $prams = mysqli_fetch_array( $m_result, MYSQLI_ASSOC ) ) {
 
-			                $ALT_TEXT = get_template_value( 'ALT_TEXT', 1 );
-			                $ALT_TEXT = str_replace( "'", "", $ALT_TEXT );
-			                $ALT_TEXT = ( str_replace( "\"", '', $ALT_TEXT ) );
-			                $js_str   = "onmouseover=\"sB(event, '" . $ALT_TEXT . "', this, " . $prams['ad_id'] . ")\" onmousemove=\"sB(event, '" . $ALT_TEXT . "', this, " . $prams['ad_id'] . ")\" onmouseout=\"hI()\" ";
-			                echo $br . '<a target="_blank" ' . $js_str . ' href="http://' . get_template_value( 'URL', 1 ) . '">' . get_template_value( 'ALT_TEXT', 1 ) . '</a>';
-			                $br = '<br>';
-		                }
+                        $ALT_TEXT = get_template_value( 'ALT_TEXT', 1 );
+                        $ALT_TEXT = str_replace( "'", "", $ALT_TEXT );
+                        $ALT_TEXT = ( str_replace( "\"", '', $ALT_TEXT ) );
+                        $js_str   = "onmouseover=\"sB(event, '" . $ALT_TEXT . "', this, " . $prams['ad_id'] . ")\" onmousemove=\"sB(event, '" . $ALT_TEXT . "', this, " . $prams['ad_id'] . ")\" onmouseout=\"hI()\" ";
+                        echo $br . '<a target="_blank" ' . $js_str . ' href="http://' . get_template_value( 'URL', 1 ) . '">' . get_template_value( 'ALT_TEXT', 1 ) . '</a>';
+                        $br = '<br>';
+                    }
 
-					?>
-                </div>
-                <div class="list-cell">
-					<?php echo $row['pixels']; ?>
-                </div>
-            </div>
-			<?php
+                    ?>
+                </td>
+                <td>
+                    <?php echo $row['pixels']; ?>
+                </td>
+        </tr>
+            <?php
 
-		}
+        }
+        ?>
+        </tbody>
+    </table>
 
-		?>
-    </div>
 <?php
 include_once( MDSROOT . "/html/footer.php" );
 ?>

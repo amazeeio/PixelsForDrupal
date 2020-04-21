99c3ec04fc3f1ebd343eae3655340035b1066dbb
eli.stone <eli.stone@amazee.com>
2020-04-21 20:59:11 +0100

    Switched over to internal JS / CSS

--- display_stats.php
+++ display_stats.php
@@ -62,8 +62,7 @@ if ($label['available_stats'] == '') {
 <html>
 <head>
     <title></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <style>
         html,body {
             background-color: transparent !important;
--- html/header.php
+++ html/header.php
@@ -36,8 +36,7 @@ $logourl = SITE_LOGO_URL;
     <meta name="Description" content="<?php echo SITE_SLOGAN; ?>">
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 </head>
 <body>
 

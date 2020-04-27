910e44e2ecdaf3eba5c094e14be5b9260b17c94e
Michael Schmid <michael@amazee.com>
2020-04-27 19:48:54 -0400

    disable header for now, it breaks firefox and other browsers

--- html/footer.php
+++ html/footer.php
@@ -20,6 +20,5 @@
 <script src="/assets/js/jquery-3.0.0.slim.min.js"></script>
 <script src="/assets/js/popper.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
-<script src="/assets/js/header.js"></script>
 </body>
 </html>

8abddaf025aca74505f201e71d6aa6482779cfc5
Michael Schmid <michael@amazee.com>
2020-04-28 23:06:42 -0400

    add startpage text to FAQ

--- faq.php
+++ faq.php
@@ -41,11 +41,27 @@ include_once( MDSROOT . "/html/header.php" );
 global $label;
 ?>
 <div class="container text-left">
+
+<p><strong>What is this?</strong></p>
+<p>You might remember the <a href='http://www.milliondollarhomepage.com/'>Million Dollar Homepage</a> from way back when. You could buy pixels and use them to post whatever you wanted - and the guy who started it made a million dollars. We thought it would be fun to make a Half Million Dollar homepage to help the Drupal Association reach their goal. Donors can purchase pixels to support the DA. You’ll get 100 pixels for every $5 you donate. You can post images and links to your pixels.  </p>
+
+<p><strong>What do I need to do?</strong></p>
+<p>Donate to #DrupalCares at <a href='https://www.drupal.org/association/donate'>https://www.drupal.org/association/donate</a>.</p>
+
+<p>Create an account on <a href='https://pixelsfordrupal.com/'>pixelsfordrupal.com</a>.</p>
+
+<p>You’ll receive a verification email. Log in and verify your account. </p>
+
+<p>You can now upload your pixels. You don’t need to do this all at once. You can upload some now, some later - you can use the whole amount at once or divide it up - it’s all up to you! You can also donate more to increase the amount of pixels you can upload. After you upload your pixels, you’ll be asked for the voucher code that was sent to you. </p>
+
+<p><strong>What can I upload?</strong></p>
+<p>It’s up to you! Uploads and links are subject to the <a href='https://www.drupal.org/dcoc'>Drupal Code of Conduct</a>, so keep it professional and kind. Upload a picture of yourself, your pet, your company logo - be creative! We will have a moderation team quickly reviewing each submission.</p>
+
 <p><strong>How do I get pixels?</strong></p>
 <p>Go to <a href="https://www.drupal.org/association/donate">https://www.drupal.org/association/donate</a> and donate!</p>
 
 <p><strong>Whose idea was this?</strong></p>
-<p>The folks at <a href="amazee.io">amazee.io</a> came up with the idea and hosted it. The site is <a href="https://milliondollarscript.com/">based on this script</a>.</p>
+<p>The folks at <a href="https://amazee.io">amazee.io</a> came up with the idea and hosted it. The site is <a href="https://milliondollarscript.com/">based on this script</a>.</p>
 
 <p><strong>Who built this?</strong></p>
 <p>The Drupal community pitched in to build this - it was a group effort!</p>
@@ -54,7 +70,7 @@ global $label;
 <p>The short version is that DrupalCon will not be held as usual, and it’s the DA’s main source of revenue for the year. You can <a href="https://www.drupal.org/association/blog/drupalcares-sustaining-the-da-through-the-covid-19-crisis">read more in their blog post</a>. </p>
 
 <p><strong>I found a bug!</strong></p>
-<p>Thanks for catching that! Please post an issue in <a href="https://github.com/amazeeio/DrupalCaresHalfMillionDollarHomepage">our GitHub issue queue</a>.</p>
+<p>Thanks for catching that! Please post an issue in <a href="https://github.com/amazeeio/PixelsForDrupal">our GitHub issue queue</a>.</p>
 </div>
 <?php
 include_once( MDSROOT . "/html/footer.php" );

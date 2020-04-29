bb5ef5db41a12f0510e6885b7bcc1edaffd903ae
Sean Hamlin <sean.hamlin@amazee.com>
2020-04-29 21:59:55 +1200

    Actually kill the session cookie when you log out.

--- users/logout.php
+++ users/logout.php
@@ -35,19 +35,24 @@ require ("../config.php");
 
 $now = (gmdate("Y-m-d H:i:s"));
 $sql = "UPDATE `users` SET `logout_date`='$now' WHERE `Username`='".mysqli_real_escape_string( $GLOBALS['connection'], $_SESSION['MDS_Username'])."'";
-      //echo $sql;
- mysqli_query($GLOBALS['connection'], $sql);
-      
+mysqli_query($GLOBALS['connection'], $sql);
 
-unset($_SESSION['MDS_ID']);
-$_SESSION['MDS_ID']='';
-$_SESSION['MDS_Domain']='';
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+  $params = session_get_cookie_params();
+  setcookie(session_name(), '', time() - 42000,
+    $params["path"], $params["domain"],
+    $params["secure"], $params["httponly"]
+  );
+}
+
+// Finally, destroy the session.
 session_destroy();
 
-if ( isset( $_COOKIE['PHPSESSID'] ) ) {
-	unset( $_COOKIE['PHPSESSID'] );
-	setcookie( 'PHPSESSID', null, - 1 );
-}
 require ('header.php');
 
 ?>
